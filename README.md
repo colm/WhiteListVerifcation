19fbedb6d47af288c03159bf4867fddbb40da99d
colm <colm@colm.com>
2016-06-06 16:14:38 +0100

    init

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 ## Repo to verify SampleOffer.sol from slock.it
 
-###First clone the Repo
+###First clone the Repo or run `get.sh`
+`git clone https://github.com/slockit/smart-contract`
 `git clone https://github.com/slockit/DAO`
+`cd DAO`
+`git checkout v1.0`
+`cd ..`
 
 ###Then run the verify script
+start geth with rpc 
+`geth --rpc console 2>> out.txt`
+run the node script to verify against the given transaction
 `nodejs --stack-size=64000 verify.js`
 
 ###Installing Required Modules
--- verify.js
+++ verify.js
@@ -1,25 +1,29 @@
-//verify SamppleOFfer
+//verify SampleOFfer
 var crypto = require('crypto');
 var fs= require('fs');
 var solc = require('solc');
 var web3 = require('web3');
 var web3 = new web3;
-params = "000000000000000000000000b01dbddfef8dedb28efbaf2b4f68acb19d520ed7000000000000000000000000159fe90ac850c895e4fd144e705923cfa042d97400000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004563918244f400000000000000000000000000000000000000000000000000000de0b6b3a76400000000000000000000000000000000000000000000000000000de0b6b3a7640000";
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:1050'));
+
+
+params = "000000000000000000000000a86adee19aab415cb31363f2390ab6e5e8acd682000000000000000000000000bb9bc244d798123fde783fcc1c72d3bb8c1894137c700cc79418d7578a9fe35e43b0b79830c645b122489113ab93658f3df571880000000000000000000000000000000000000000000001b1ae4d6e2ef500000000000000000000000000000000000000000000000000005677125153ec0c0000000000000000000000000000000000000000000000000000f2dc7d47f1560000";
+transaction = "0x8a2f0f277b79c46e8b718a49ed60bbc169c72799b5db35efebb0f8eea2e7a58f"
+
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 var input = {
-   'SampleOffer.sol' : fs.readFileSync('./DAO/SampleOffer.sol', 'utf8'),
-   'Token.sol' : fs.readFileSync('DAO/Token.sol', 'utf8'),
-   'TokenCreation.sol' : fs.readFileSync('DAO/TokenCreation.sol', 'utf8'),
-   'ManagedAccount.sol' : fs.readFileSync('DAO/ManagedAccount.sol', 'utf8'),
-   'DAO.sol' : fs.readFileSync('DAO/DAO.sol', 'utf8')
+   'DAOSecurity.sol' : fs.readFileSync('smart-contract/DAOSecurity.sol', 'utf8'),
+   'DAO/Token.sol' : fs.readFileSync('DAO/Token.sol', 'utf8'),
+   'DAO/TokenCreation.sol' : fs.readFileSync('DAO/TokenCreation.sol', 'utf8'),
+   'DAO/ManagedAccount.sol' : fs.readFileSync('DAO/ManagedAccount.sol', 'utf8'),
+   'DAO/DAO.sol' : fs.readFileSync('DAO/DAO.sol', 'utf8')
 };
 
 output = solc.compile({sources: input}, 1);
-hash = crypto.createHash('sha256').update(output.contracts.SampleOffer.bytecode).digest('hex');
-code = output.contracts.SampleOffer.bytecode;
 
-trans = web3.eth.getTransaction("0xc18f28fffdc2f9d6a6fa73eb05e309ac0a321fa3be7deef7abcc24d00c1e4293");
+hash = crypto.createHash('sha256').update(output.contracts.DAOSecurity.bytecode).digest('hex');
+code = output.contracts.DAOSecurity.bytecode;
+trans = web3.eth.getTransaction(transaction);
 
-console.log(crypto.createHash('sha256').update(output.contracts.SampleOffer.bytecode + params).digest('hex'));
+console.log(crypto.createHash('sha256').update("0x"+output.contracts.DAOSecurity.bytecode + params).digest('hex'));
 console.log(crypto.createHash('sha256').update(trans.input).digest('hex'));
