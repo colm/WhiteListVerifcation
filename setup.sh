431aee0a30b2c0a7d2fe15e2d9546025adc88d7d
colm <colm@colm.com>
2016-06-07 20:31:19 +0100

    added setup

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+npm install crypto
+npm install solc
+npm install web3
+
+git clone https://github.com/slockit/smart-contract
+git clone https://github.com/slockit/DAO
+cd DAO
+git checkout v1.0
+cd ..
+
